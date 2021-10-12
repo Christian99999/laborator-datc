fe782d6fc205468f1d6ad92e3d87cea6a3243808
Christian99999 <christian.andreas1999@gmail.com>
2021-10-12 21:47:19 +0300

    Trimitere Tema 2

---
+++ Streitmatter-Andreas-Christian/L02/Lab2Datc/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+using Repositories;
+
+namespace Lab2Datc.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return StudentsRepo.Students;
+        }
+    }
+}
